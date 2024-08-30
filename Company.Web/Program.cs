69621573a94c18cbbd67110f2d9508c9c3d26eb2
nourawael <nourawael99@gmail.com>
2024-08-30 16:13:01 +0300

    DbContext & Connection String

--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -1,3 +1,6 @@
+using Company.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company.Web
 {
     public class Program
@@ -9,6 +12,11 @@ namespace Company.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<CompanyDbContext>(options=> 
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
