7b38cef938739116fa75fc2261455bf4c16b8b5e
nourawael <nourawael99@gmail.com>
2024-09-04 17:43:27 +0300

    DepartmentController [create(Action&View)]

--- Company.Web/Controllers/DepartmentController.cs
+++ Company.Web/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
-﻿using Company.Repository.Interfaces;
+﻿using Company.Data.Entities;
+using Company.Repository.Interfaces;
 using Company.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,10 +13,37 @@ namespace Company.Web.Controllers
         {
             _departmentService = departmentService;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             var departemnts = _departmentService.GetAll();
             return View(departemnts);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department department) {
+            try {
+                if (ModelState.IsValid)
+                {
+                    _departmentService.Add(department);
+                    //return RedirectToAction("Index");
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("DepartmentError", "ValidationErrors");
+
+                return View(department);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("DepartmentError", ex.Message);
+
+                return View(department);
+            }
+            
         }
     }
 }
