70e1a9fe731ae00615ad94a396db7cc72cde3944
nourawael <nourawael99@gmail.com>
2024-09-04 17:53:29 +0300

    save changes

--- Company.Repository/Repositories/GenericRepository.cs
+++ Company.Repository/Repositories/GenericRepository.cs
@@ -17,10 +17,17 @@ namespace Company.Repository.Repositories
             _context = context;
         }
         public void Add(T entity)
-        => _context.Set<T>().Add(entity);
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
 
         public void Delete(T entity)
-        => _context.Set<T>().Remove(entity);
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+
+        }
 
         public IEnumerable<T> GetAll()
         => _context.Set<T>().ToList();
