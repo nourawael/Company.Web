6094ae434fafbb80ecfd06928e27199166e36ba4
nourawael <nourawael99@gmail.com>
2024-09-07 18:26:17 +0300

    EmployeeController (Create)

--- Company.Web/Controllers/EmployeeController.cs
+++ Company.Web/Controllers/EmployeeController.cs
@@ -29,36 +29,55 @@ namespace Company.Web.Controllers
             return View(employees);
 
         }
-        //[HttpGet]
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-        //[HttpPost]
-        //public IActionResult Create()
-        //{
-        //}
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+           
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _employeeService.Add(employee);
+                    //return RedirectToAction("Index");
+                    return RedirectToAction(nameof(Index));
+                }
+              
+
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+              
+
+                return View(employee);
+            }
+        }
 
         //public IActionResult Details()
         //{
-            
+
         //}
 
         //[HttpGet]
 
         //public IActionResult Update(int? id)
         //{
-            
+
         //}
         //[HttpPost]
         //public IActionResult Update(int? id, Department department)
         //{
-           
+
         //}
 
         //public IActionResult Delete(int? id)
         //{
-            
+
 
         //}
 
