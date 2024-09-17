7693866dda75608e9ccf27a18b1f61e61c66b83e
nourawael <nourawael99@gmail.com>
2024-09-17 14:24:51 +0300

    AccountController(SignOut)

--- Company.Web/Controllers/AccountController.cs
+++ Company.Web/Controllers/AccountController.cs
@@ -72,5 +72,11 @@ namespace Company.Web.Controllers
 
         }
         #endregion
+
+        public new async Task<IActionResult> SignOut() 
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
     }
 }
