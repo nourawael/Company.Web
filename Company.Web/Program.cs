25514adf4f4bc75e0b01546427d621b927e60883
nourawael <nourawael99@gmail.com>
2024-09-17 13:41:38 +0300

    part01 Account Controller (SignUp- View)

--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -79,7 +79,7 @@ namespace Company.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=SignUp}");
 
             app.Run();
         }
