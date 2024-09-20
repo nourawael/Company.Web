b334c5e90e578163e3ca2cbdd2d9902db3a5b4a0
nourawael <nourawael99@gmail.com>
2024-09-20 17:02:07 +0300

    UserController (Update)

--- Company.Web/Controllers/UserController.cs
+++ Company.Web/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Company.Data.Entities;
+using Company.Service.Interfaces.Department.Dto;
+using Company.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,10 +10,14 @@ namespace Company.Web.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger _logger;
 
-        public UserController(UserManager<ApplicationUser> userManager)
+        public UserController(
+            UserManager<ApplicationUser> userManager,
+            ILogger<UserController> logger)
         {
             _userManager = userManager;
+            _logger = logger;
         }
         public async Task<IActionResult> Index(string searchInp)
         {
@@ -32,7 +38,62 @@ namespace Company.Web.Controllers
             if (user is null)
                 return NotFound();
 
+            if (viewName == "Update") 
+            {
+                var userViewModel = new UserUpdateViewModel
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                };
+                return View(viewName,userViewModel);
+            }
+
             return View(viewName, user);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Update(string id)
+        {
+            return await Details(id, "Update");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, UserUpdateViewModel applicationUser)
+        {
+            if (id != applicationUser.Id)
+                return NotFound();
+
+            if (ModelState.IsValid) 
+            {
+                try
+                {
+                    var user = await _userManager.FindByIdAsync(id);
+
+                    if (user is null)
+                        return NotFound();
+
+                    user.UserName = applicationUser.UserName;
+                    user.NormalizedUserName = applicationUser.UserName.ToUpper();
+
+                    var result = await _userManager.UpdateAsync(user);
+
+                    if (result.Succeeded) 
+                    {
+                        _logger.LogInformation("User Updated Successfully");
+                        return RedirectToAction("Index");
+                    }
+                    foreach (var item in result.Errors)
+                        _logger.LogError(item.Description);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message);
+
+                }
+            }
+
+            return View(applicationUser);
         }
 
     }
